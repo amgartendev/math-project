37e93bfef1839f55e0d5da7805e71a070a652213
João Amgarten <joaoamgarten2003@gmail.com>
2022-08-20 17:34:47 +0100

    build: add Calculate class

---
+++ main.py
@@ -0,0 +1,14 @@
+from models.calculate import Calculate
+
+
+def main() -> None:
+    score: int = 0
+    play(score)
+
+
+def play(score: int) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/calculate.py
@@ -0,0 +1,56 @@
+from random import randint
+
+
+class Calculate:
+    def __init__(self: object, difficulty: int, /) -> None:
+        self.__difficulty: int = difficulty
+        self.__value1: int = self._generate_value
+        self.__value2: int = self._generate_value
+        self.__operation: int = randint(1, 3)  # 1 - Addition, 2 - Subtraction, 3 - Multiplication
+        self.__result: int = self._generate_result
+
+    @property
+    def difficulty(self: object) -> int:
+        return self.__difficulty
+
+    @property
+    def value1(self: object) -> int:
+        return self.__value1
+
+    @property
+    def value2(self: object) -> int:
+        return self.__value2
+
+    @property
+    def operation(self: object) -> int:
+        return self.__operation
+
+    @property
+    def result(self: object) -> int:
+        return self.__result
+
+    @property
+    def _generate_value(self: object) -> int:
+        pass
+
+    @property
+    def _generate_result(self: object) -> int:
+        pass
+
+    def __str__(self: object) -> str:
+        op: str = ''
+        if self.operation == 1:
+            op = 'Addition'
+        elif self.operation == 2:
+            op = 'Subtraction'
+        elif self.operation == 3:
+            op = 'Multiplication'
+        else:
+            op = 'Error: Invalid Option'
+        return f'Value 1: {self.value1}\nValue 2: {self.value2}\nDifficulty: {self.difficulty}\nOperation: {op}'
+
+    def check_result(self: object, user: int) -> bool:
+        pass
+
+    def show_operation(self: object, user: int) -> None:
+        pass
